744b9df963fc7722114a2b487a0eb24f6cd0801c
padac78 <72388269+padac78@users.noreply.github.com>
2023-02-14 11:32:56 +0100

    commit_flask

--- pyth_test.py
+++ pyth_test.py
@@ -1,4 +1,10 @@
-print("Hello, World!")
+from flask import Flask
 
+app = Flask(__name__)
 
-print("coucou les loulous :)")
+@app.route('/raouf/pa', methods=['GET'])
+def hello_world():
+    return 'Joyeux Anniversaire Leny!'
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8080)
